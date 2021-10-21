aa48d15b1a7f6f1a6d193acf1a3845001c251705
Martin Peka <martin.peka@argo22.com>
2021-10-21 13:23:20 +0200

    Update harvest user script to new html struct

--- src/Fix Harvest/Fix Harvest.user.js
+++ src/Fix Harvest/Fix Harvest.user.js
@@ -69,9 +69,9 @@ function highlightMissCat() {
 
     var rows = document.querySelectorAll('tr[id^=timesheet_day_entry_]');
     rows.forEach(function(row) {
-        var projectEl = row.querySelectorAll('span.project')[0];
+        var projectEl = row.querySelectorAll('div.project-client')[0].children[0];
         var project = projectEl.innerHTML;
-        var msg = row.querySelectorAll('span.notes')[0].children[0].innerHTML;
+        var msg = row.querySelectorAll('div.notes')[0].children[0].innerHTML;
 
         var ticketCodeMatch = false;
         [
